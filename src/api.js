123fc4d485d17d67304e1cbaeb7d922dbb3053c5
Facundo Ibarzabal <facundoibarzabal@gmail.com>
2022-11-18 00:38:51 -0300

    init

--- src/api.js
+++ src/api.js
@@ -3,12 +3,34 @@ const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
+var Twit = require('twit')
+require('dotenv').config()
 
 router.get("/", (req, res) => {
   res.json({
     hello: "hi!"
   });
 });
+
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  var T = new Twit({
+    consumer_key:         process.env.C_KEY,
+    consumer_secret:      process.env.C_SECRET_KEY,
+    access_token:         process.env.A_TOKEN,
+    access_token_secret:  process.env.A_SECRET_TOKEN,
+    timeout_ms:           60*1000,
+    strictSSL:            true,
+  });
+
+  T.post('statuses/update', { status: req.body.message }, function(err, data, response) {
+    console.log(data)
+    console.log( `req ${req.body.message}, res ${response}`)
+  })
+
+  res.send();
+});
+
 
 app.use(`/.netlify/functions/api`, router);
 
